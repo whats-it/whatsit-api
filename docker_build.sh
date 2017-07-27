f8ab4bafef398879d80441fb0f611cac833961a6
Youngbok Yoon <master@bluehack.net>
2017-07-27 11:11:20 +0900

    [WHATSIT-2] Changing image using namespace on Kubernetes

--- docker_build.sh
+++ docker_build.sh
@@ -4,12 +4,12 @@ HOSTNAME="gcr.io"
 TAG=$1
 PROJECT_ID="whatsit-174908"
 IMAGE="whatsit-api"
+NAMESPACE="whatsit"
 
 docker build -t $IMAGE:$TAG .
 docker tag $IMAGE:$TAG $HOSTNAME/$PROJECT_ID/$IMAGE:$TAG
 gcloud docker -- push $HOSTNAME/$PROJECT_ID/$IMAGE:$TAG
 gcloud container images list --repository=$HOSTNAME/$PROJECT_ID
 
-#kubectl set image deployment/$HOSTNAME/$PROJECT_ID $IMAGE=$HOSTNAME/$PROJECT_ID/$IMAGE:$TAG
-#kubectl rollout status deployment/$HOSTNAME/$PROJECT_ID/$IMAGE
-#kubectl delete pods -l name=$HOSTNAME/$PROJECT_ID/$IMAGE
+kubectl --namespace=$NAMESPACE set image deployment/$IMAGE $IMAGE=$HOSTNAME/$PROJECT_ID/$IMAGE:$TAG
+kubectl --namespace=$NAMESPACE rollout status deployment/$IMAGE
